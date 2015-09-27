c62cb574fb4cb4935ee607eaac527bcc34e077ad
Naveen <naveen.venayak@gmail.com>
2015-09-27 12:28:35 -0400

    Completed data parsing script. Need to restructure objects

--- importDataScript.py
+++ importDataScript.py
@@ -1,238 +1,83 @@
 __author__ = 'Naveen'
 
+####### Import packages
 from DataObject import *
-
 from pyexcel_xlsx import get_data
 
-#data = get_data("test1.xlsx")
-data = get_data("2015.05.19.LactateStoryData.xlsx")
+####### Define some constants
+fileName = "2015.05.19.LactateStoryData.xlsx"
+substrateName = 'Glucose'
+titerDataSheetName = "titers"
 
+
+###############################
+
+####### Get data from xlsx file
+data = get_data(fileName)
+
+####### Check for correct data for import
 # if 'OD' not in data.keys():
 #     raise Exception("No sheet named 'OD' found")
 
 if 'titers' not in data.keys():
     raise Exception("No sheet named 'titers' found")
 
-substrate = "Glucose"
-titerDataSheetName = "titers"
-
+######## Initialize variables
 titerNameColumn = dict()
 for i in range(1,len(data[titerDataSheetName][2])):
     titerNameColumn[data[titerDataSheetName][2][i]] = i
 
-
-#Build time point objects from titer HPLC data
-#timePointCollection = [ [] for i in range(0,len(titerName))]
-
 tempTimePointCollection = dict()
 for names in titerNameColumn:
-    print(type(names))
     tempTimePointCollection[names] = []
 
 timePointCollection = []
-
+singleExperimentDataList = []
 skippedLines = 0
+
+######## Parse the titer data into single experiment object list
 for i in range(6, len(data['titers'])):
-    #Parse the identifier
-    if type("asdf") == type(data['titers'][i][0]):
-        tempParsedIdentifier = data['titers'][i][0].split(',')
-        if len(tempParsedIdentifier) >= 3:
+    if type("asdf") == type(data['titers'][i][0]):  #Check if the data is a string
+        tempParsedIdentifier = data['titers'][i][0].split(',')  #Parse the string using comma delimiter
+        if len(tempParsedIdentifier) >= 3:  #Ensure corect number of identifiers TODO make this general
             tempRunIdentifierObject = runIdentifier()
             tempRunIdentifierObject.strainID = tempParsedIdentifier[0]
             #tempRunIdentifierObject.identifier1 = tempParsedIdentifier[1]
             # tempRunIdentifierObject.identifier2 = tempParsedIdentifier[2]
             tempRunIdentifierObject.replicate = tempParsedIdentifier[1]
             tempRunIdentifierObject.t = tempParsedIdentifier[2]
-            #print(tempRunIdentifierObject.returnUniqueID())
 
-
-
+            singleExperimentDataList.append(singleExperimentData())
             for key in tempTimePointCollection:
-                #print("the key is",key)
                 tempTimePointCollection[key] = timePoint(tempRunIdentifierObject, key, tempRunIdentifierObject.time, data['titers'][i][titerNameColumn[key]])
+                if key == substrateName:
+                    print(tempTimePointCollection[key])
+                    singleExperimentDataList[-1].substrate = tempTimePointCollection[key]
+                else:
+                    singleExperimentDataList[-1].products[key] = tempTimePointCollection[key]
 
             timePointCollection.append(tempTimePointCollection.copy())
-            #print(timePointCollection[len(timePointCollection)-1]["Glucose"].getUniqueTimePointID())
-            #print(timePointCollection[len(timePointCollection)-1]["Glucose"].titer)
-
-            print("tempCollection: ",tempTimePointCollection["Glucose"].getUniqueTimePointID())
-
-            for  r in range(0,len(timePointCollection)):
-                print(timePointCollection[r]["Glucose"].getUniqueTimePointID())
-                print(timePointCollection[r]["Glucose"].titer)
-
-            #input("Press Enter to continue...")
         else:
             skippedLines = skippedLines + 1
-            #print(tempRunIdentifierObject.t)
     else:
         skippedLines = skippedLines+1
-
-print("Number of lines skipped ",skippedLines )
-
-# for  i in range(0,len(timePointCollection)):
-#     print(timePointCollection[i]["Glucose"].getUniqueTimePointID())
-#     print(timePointCollection[i]["Glucose"].titer)
+print("Number of lines skipped ",skippedLines)
 
 
-
-
-
+######## Combine replicate experiments
 uniqueTimePointCollection = dict()
 #Find unique timePointIdentifiers
-for i in timePointCollection:
-    if i["Glucose"].getUniqueTimePointID() in uniqueTimePointCollection:
-        uniqueTimePointCollection[i["Glucose"].getUniqueTimePointID()].append(i)  #TODO make this general
-        #for key in uniqueTimePointCollection[i["Glucose"].getUniqueTimePointID()].keys():
-
+for i in singleExperimentDataList:
+    if i.getUniqueTimePointID() in uniqueTimePointCollection:
+        uniqueTimePointCollection[i.getUniqueTimePointID()].append(i)
     else:
-        print(i["Glucose"].getUniqueTimePointID())
-        uniqueTimePointCollection[i["Glucose"].getUniqueTimePointID()] = []
-        # for key in uniqueTimePointCollection[i["Glucose"].getUniqueTimePointID()].keys():
-        #     uniqueTimePointCollection[i["Glucose"].getUniqueTimePointID()][key]  #TODO make this general
+        uniqueTimePointCollection[i.getUniqueTimePointID()] = [i]
 
+######### Build the replicate objects
+replicateExperimentObjectList = dict()
+replicateExperimentObject()
 
-
-    # flag = 0
-    # #if not uniqueTimePointCollection.has_key(i.getUniqueTimePointID()):
-    # #else:
-    #
-    #
-    # for key in uniqueTimePointCollection.keys():
-    #     if key == i.getUniqueTimePointID():
-    #         uniqueTimePointCollection[i.getUniqueTimePointID] = i
-    #         flag = 1
-    #     if flag == 0
-
-
-
-# for key in titerNameColumn:
-#     uniqueTimePointCollection[0][key] = []
-#
-# for i in range(0,len(timePointCollection['Glucose'])): #### Should be generalized
-#     #Loop through all the experimentObjects to check if one with the same conditions exists
-#     flag = 0
-#     for j in range(0,len(uniqueTimePointCollection['Glucose'])):
-#         if uniqueTimePointCollection['Glucose'][j].getUniqueTimePointID() == timePointCollection['Glucose'][i].getUniqueTimePointID():
-#             for key in timePointCollection:
-#                 uniqueTimePointCollection[key][j].append(timePointCollection[key][i])
-#             flag = 1
-#             print("flag is 1")
-#             break
-#     if flag == 0:
-#         for key in timePointCollection:
-#             #print(i," "," ",key)
-#             print("flag is 0")
-#             uniqueTimePointCollection[key].append(timePointCollection[key][i])
-#         #print(uniqueTimePointCollection[0][i].runIdentifier.strainID)
-#
-#
-#
-
-
-
-
-
-
-
-
-
-
-
-# for i in range(5,len(uniqueTimePointCollection)):
-#     print(uniqueTimePointCollection[i].getUniqueTimePointID())
-#
-# for i in range(0, len(data['OD'])):
-#     #Parse the identifier
-#     tempParsedIdentifier = data['OD'][i][0].split(',')
-#     tempRunIdentifierObject = runIdentifier
-#     tempRunIdentifierObject.strainID = tempParsedIdentifier[0]
-#     tempRunIdentifierObject.identifier1 = tempParsedIdentifier[1]
-#     tempRunIdentifierObject.identifier2 = tempParsedIdentifier[2]
-#     tempRunIdentifierObject.replicate = tempParsedIdentifier[3]
-#     #ODs[i][0].time = tempParsedIdentifier[4]
-#
-#     timeCourseObjectList[i] = timeCourseObject(tempParsedIdentifier,data['ODt'][0],data['OD'][i][1:len(data['OD'][i])])
-#
-# replicateObjectList = []
-#
-#
-#
-#
-#
-# #Loop through each timeCourseObject
-# for i in len(timeCourseObjectList):
-#     #Loop through all the experimentObjects to check if one with the same conditions exists
-#     flag = 0
-#     for j in len(replicateObjectList):
-#         if timeCourseObjectList[i].runIdentifier.returnUniqueID() == replicateObjectList[j].runIdentifier.returnUniqueID():
-#             replicateObjectList[j].addReplicateExperiment(timeCourseObjectList[i])
-#             flag = 1
-#             break
-#     if flag == 0:
-#         replicateObjectList.append(timeCourseObjectList[i])
-
-
-
-
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-# #Miscellaneous
-# t = np.array([0,2.5,4,6,9,12,14,17,20,23])
-# data = np.array([0.0633166666666667,0.107116668166667,0.126316665500000,0.162466665000000,0.254633329833333,0.290883332500000,0.305849996666667,0.311133335000000,0.312383334000000,0.318266670000000])
-#
-# dataObjectTest = timeCourseObject(runIdentifier(), t, data)
-# print(dataObjectTest.exponentialRate)
-#
-#
-#
-# from pyexcel_xlsx import get_data
-#
-# #Data must be in the following format:
-# #Sheet name: OD, col 1: names, col2:
-# #Sheet name: titers
-#
-#
-#
-# for key in data.keys():
-#     print data.get(key)
-# for key, elem in data.items():
-#     print key, elem
-# for i in data:
-#      print i, data[i]
-# for key in data.iterkeys():
-#     print(key, data[key])
-# for c in a:
-#     print(c)
-#
-#
-# data = get_data("test1.xlsx")
-# data['Sheet1'].split(",")
-# for key in data.keys():
-#     print(data[key])
-# # Import the data
-#
-# #Go through the names array
-#     #Parse the information: Strain ID, Identifier1 (Plasmid), Identifier2, condition
-#     #Check if singleExperimentData exists for the dataType
-#         #if yes, add to singleExperimentData
-#
-#         #if no, make a new singleExperimentData object
-#
-#
-#
-#
-#
-#
-# if 'Sheetx' in data.keys():
-#     print("yes")
-# else:
-#     print("no")
+for key in uniqueTimePointCollection.keys():
+    replicateExperimentObjectList[key] = replicateExperimentObject()
+    for i in uniqueTimePointCollection[key]:
+        replicateExperimentObjectList[key].addReplicateExperiment(i)
