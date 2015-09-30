b4a118a7f9cba0ae49286bdb0c246e84c8f40d96
Naveen <naveen.venayak@gmail.com>
2015-09-29 23:26:18 -0400

    First working pass of parse and plot.

--- DataObject.py
+++ DataObject.py
@@ -4,12 +4,13 @@ import numpy as np
 import scipy.optimize as opt
 
 class runIdentifier(object):
-
-    strainID = ''
-    identifier1 = ''
-    identifier2 = ''
-    replicate = 0
-    time = 0
+    #Base runIdentifier object
+    def __init__(self):
+        self.strainID = ''
+        self.identifier1 = ''
+        self.identifier2 = ''
+        self.replicate = None
+        self.time = None
 
     def returnUniqueID(self):
         return self.strainID+self.identifier1+self.identifier2
@@ -22,20 +23,38 @@ class timePoint(object):
         self.titer = titer
 
     def getUniqueTimePointID(self):
-        return self.runIdentifier.strainID+self.runIdentifier.identifier1+self.runIdentifier.identifier2+self.runIdentifier.replicate
+        return self.runIdentifier.strainID+self.runIdentifier.identifier1+self.runIdentifier.identifier2+str(self.runIdentifier.replicate)
 
-class titerData():
-    def __init__(self, runID, prodID, t, data):
-        self.t = t
-        self.prodID = prodID
-        self.data = data
-        self.runID = runID
+class titerObject():
+    #def __init__(self, runID, prodID, t, data):
+    def __init__(self):
+        self.timePointList = []
 
-class timeCourseObject(titerData):
+    def addTimePoint(self, timePoint):
+        raise(Exception("No addTimePoint method defiend in the child"))
+
+    def getTimeCourseID(self):
+        return self.timePointList[0].runIdentifier.strainID+\
+               self.timePointList[0].runIdentifier.identifier1+\
+               self.timePointList[0].runIdentifier.identifier2+\
+               str(self.timePointList[0].runIdentifier.replicate)
+
+    def getReplicateID(self):
+        return self.timePointList[0].runIdentifier.strainID+self.timePointList[0].runIdentifier.identifier1+self.timePointList[0].runIdentifier.identifier2
+
+class timeCourseObject(titerObject):
     #Object contains one time vector and one data vector
-    def __init__(self, runID, prodID, t, data):
-        titerData.__init__(self, runID, prodID, t, data)
-        self.exponentialRate = self.calcExponentialRate(t, data)
+    def __init__(self):
+        titerObject.__init__(self)
+        self.timeVec = None
+        self.dataVec = None
+
+    def addTimePoint(self, timePoint):
+        self.timePointList.append(timePoint)
+        self.timePointList.sort(key=lambda timePoint: timePoint.t)
+        self.timeVec = np.array([timePoint.t for timePoint in self.timePointList])
+        self.dataVec = np.array([timePoint.titer for timePoint in self.timePointList])
+        #print(self.timeVec)
 
     def calcExponentialRate(self, t, data):
         #Define the growth equation to fit parameters
@@ -45,11 +64,19 @@ class timeCourseObject(titerData):
         popt, pcov = opt.curve_fit(growthEquation,t,data)
         return popt
 
-class endPointObject(titerData):
+class endPointObject(titerObject):
     def __init__(self, runID, t, data):
-        titerData.__init__(self, runID, t, data)
+        titerObject.__init__(self, runID, t, data)
         #self.data = data
         #self.runID = runID
+    def addTimePoint(self, timePoint):
+        if len(self.timePointList) < 2:
+            self.timePointList.append(timePoint)
+        else:
+            raise(Exception("Cannot have more than two timePoints for an endPoint Object"))
+
+        if len(self.timePointList) == 2:
+            self.timePointList.sort(key=lambda timePoint: timePoint.t)
 
 class productsObject(object):
     def __init__(self, names, timeCourses):
@@ -60,15 +87,78 @@ class productsObject(object):
 class singleExperimentData(object):
     #Object contains a series of timeCourseObjects related to a single experiment
     def __init__(self):
-        # self.productNames = ['succinate','formate','acetate','ethanol','lactate']
-        self.OD = []
-        self.substrate = []
-        #self.substrateConsumed = self.calcSubstrateConsumed()
+        self.__t = np.array([])
+        self.__OD = []
+        self.__substrate = []
         self.products = dict()
         #self.yields = self.calcYield()
+        #self.substrateConsumed = self.calcSubstrateConsumed()
+
+    @property
+    def OD(self):
+        return self.__OD
+
+    @OD.setter
+    def OD(self, OD):
+        self.__OD = OD
+        self.checkTimeVectors()
+
+    @property
+    def substrate(self):
+        return self.__substrate
+
+    @substrate.setter
+    def substrate(self, substrate):
+        self.__substrate = substrate
+        self.checkTimeVectors()
+
+    @property
+    def t(self):
+        return self.__t
+
+    @t.setter
+    def t(self, t):
+        self.__t = t
+
+    # @property
+    # def products(self):
+    #     return self.__products
+    #
+    # @products.setter
+    # def products(self, products):
+    #     self.__products = products
+
+    def checkTimeVectors(self):
+        t = []
+        flag = 0
+        if self.OD:
+            t.append(self.OD.timeVec)
+        if self.substrate != None:
+            try:
+                t.append(self.substrate.timeVec)
+            except:
+                a = 0
+
+        if self.products:
+            for key in self.products:
+                t.append(self.products[key].timeVec)
+
+        for i in range(len(t)-1):
+            #print(t[i])
+            if (t[i] != t[i+1]).all():
+                flag = 1
+
+
+        if flag==1:
+            raise(Exception("Time vectors within an experiment don't match, must implement new methods to deal with this type of data (if even possible)"))
+        else:
+            self.__t = self.substrate.timeVec
 
     def getUniqueTimePointID(self):
-        return self.substrate.runIdentifier.strainID+self.substrate.runIdentifier.identifier1+self.substrate.runIdentifier.identifier2+self.substrate.runIdentifier.replicate
+        return self.substrate.runIdentifier.strainID+self.substrate.runIdentifier.identifier1+self.substrate.runIdentifier.identifier2+str(self.substrate.runIdentifier.replicate)
+
+    def getUniqueReplicateID(self):
+        return self.substrate.getReplicateID()
 
     def calcSubstrateConsumed(self):
         substrateConsumed = []
@@ -83,33 +173,102 @@ class singleExperimentData(object):
             yields[productIndex,:] = np.divide(self.products.data[productIndex,1:len(self.products)],self.substrateConsumed.data[1:len(self.substrateConsumed)])
         return yields
 
+
+class singleExperimentDataShell(singleExperimentData):
+    # def __init__(self):
+    #     replicateExperimentObject.__init__(self)
+
+    @singleExperimentData.substrate.setter
+    def substrate(self, substrate):
+        self.__substrate = substrate
+
+    @singleExperimentData.OD.setter
+    def OD(self, OD):
+        self.__OD = OD
+
+    # @singleExperimentData.products.setter
+    # def products(self, products):
+    #     self.__products = products
+
+
+
+
 class replicateExperimentObject(object):
     #Calculates statistics based on replicates
     def __init__(self):
-        #self.replicates = replicates
-        #self.replicatesToUse = replicatesToUse
-        #self.uniqueID = replicates.returnUniqueID()
-        self._timeCourseObjectList = []
-        #self.yieldAverages, self.titerAverages, self.ODaverages, self.growthRateAverage = calcAverages()
-        self.checkReplicateUniqueIDMatch()
+        self.avg = singleExperimentDataShell()
+        self.std = singleExperimentDataShell()
+        self.t = None
+        self.singleExperimentList = []
+        #self.useReplicate = dict()
+        #self.checkReplicateUniqueIDMatch()
 
     def checkReplicateUniqueIDMatch(self):
-        for i in range(len(self.timeCourseObjectList)):
-            if self.replicates[i].returnUniqueID() != self.replicates[i].returnUniqueID():
-                raise(Exception,"the replicates do not have the same uniqueID, either the uniqueID includes too much information or the strains don't match")
+        for i in range(len(self.singleExperimentList)-1):
+            if self.singleExperimentList[i].getUniqueReplicateID() != self.singleExperimentList[i+1].getUniqueReplicateID():
+                raise(Exception("the replicates do not have the same uniqueID, either the uniqueID includes too much information or the strains don't match"))
 
+            if (self.singleExperimentList[i].t != self.singleExperimentList[i+1].t).all():
+                raise(Exception("time vectors don't match within replicates"))
+            else:
+                self.t = self.singleExperimentList[i].t
+
+            if len(self.singleExperimentList[i].substrate.dataVec) != len(self.singleExperimentList[i+1].substrate.dataVec):
+                print("Time Vector 1: ",self.singleExperimentList[i].t,"\nTime Vector 2: ",self.singleExperimentList[i+1].t)
+                print("Vector 1: ",self.singleExperimentList[i].substrate.dataVec,"\nVector 2: ",self.singleExperimentList[i+1].substrate.dataVec)
+                raise(Exception("length of substrate vectors do not match"))
+
+            for key in self.singleExperimentList[i].products:
+                if len(self.singleExperimentList[i].products[key].dataVec) != len(self.singleExperimentList[i+1].products[key].dataVec):
+                    raise(Exception("length of product vector "+str(key)+" do not match"))
 
     def addReplicateExperiment(self, newReplicateExperiment):
-        self.timeCourseObjectList.append(newReplicateExperiment)
-        checkReplicateUniqueIDMatch(self)
-        calcAverageandDev(self)
+        self.singleExperimentList.append(newReplicateExperiment)
+        self.checkReplicateUniqueIDMatch()
+        self.calcAverageAndDev()
 
     #Calculate averages
-    def calcAveragesAndDev(self, data):
-        # TODO timePoint,
-        avg = np.mean(replicates.yields[replicatesToUse,])
-        std = np.std(replicates.yields[replicatesToUse,])
-        return avg, std
+    def calcAverageAndDev(self):
+        #First check what data exists
+        ODflag = 0
+        productFlag = 0
+        substrateFlag = 0
+        for singleExperiment in self.singleExperimentList:
+            # TODO timePoint,
+            if singleExperiment.OD:
+                temp=0
+            if singleExperiment.products:
+                productFlag = 1 #TODO need to make generic for different products in different expts
+            if singleExperiment.substrate:
+                substrateFlag=1
+
+        if ODflag == 1:
+            self.avg.OD = np.mean([singleExperimentObject.OD.dataVec for singleExperimentObject in self.singleExperimentList], axis=0)
+            self.std.OD = np.std([singleExperimentObject.OD.dataVec for singleExperimentObject in self.singleExperimentList], axis=0)
+
+        if productFlag == 1:
+            for key in self.singleExperimentList[0].products:
+                # for singleExperimentObject in self.singleExperimentList:
+                #     for timePoint in singleExperimentObject.products[key].timePointList:
+                #             print(timePoint.getUniqueTimePointID())
+                #
+
+                # print("Next: ",[singleExperimentObject.products[key].timeVec for singleExperimentObject in self.singleExperimentList])
+
+                self.avg.products[key] = np.mean([singleExperimentObject.products[key].dataVec for singleExperimentObject in self.singleExperimentList],axis=0)
+                self.std.products[key] = np.std([singleExperimentObject.products[key].dataVec for singleExperimentObject in self.singleExperimentList],axis=0)
+
+        if substrateFlag == 1:
+            # for singleExperimentObject in self.singleExperimentList:
+            #         for timePoint in singleExperimentObject.substrate.timePointList:
+            #                 print(timePoint.getUniqueTimePointID())
+            #
+            # print("substrateNext: ",[singleExperimentObject.substrate.dataVec for singleExperimentObject in self.singleExperimentList])
+
+            self.avg.substrate = np.mean([singleExperimentObject.substrate.dataVec for singleExperimentObject in self.singleExperimentList], axis=0)
+            self.std.substrate = np.std([singleExperimentObject.substrate.dataVec for singleExperimentObject in self.singleExperimentList], axis=0)
+
+
 
     #Calculate the production rate average
 
--- importDataScript.py
+++ importDataScript.py
@@ -3,6 +3,7 @@ __author__ = 'Naveen'
 ####### Import packages
 from DataObject import *
 from pyexcel_xlsx import get_data
+import matplotlib.pyplot as plt
 
 ####### Define some constants
 fileName = "2015.05.19.LactateStoryData.xlsx"
@@ -32,11 +33,10 @@ for names in titerNameColumn:
     tempTimePointCollection[names] = []
 
 timePointCollection = []
-singleExperimentDataList = []
 skippedLines = 0
 
 ######## Parse the titer data into single experiment object list
-for i in range(6, len(data['titers'])):
+for i in range(4, len(data['titers'])):
     if type("asdf") == type(data['titers'][i][0]):  #Check if the data is a string
         tempParsedIdentifier = data['titers'][i][0].split(',')  #Parse the string using comma delimiter
         if len(tempParsedIdentifier) >= 3:  #Ensure corect number of identifiers TODO make this general
@@ -44,40 +44,70 @@ for i in range(6, len(data['titers'])):
             tempRunIdentifierObject.strainID = tempParsedIdentifier[0]
             #tempRunIdentifierObject.identifier1 = tempParsedIdentifier[1]
             # tempRunIdentifierObject.identifier2 = tempParsedIdentifier[2]
-            tempRunIdentifierObject.replicate = tempParsedIdentifier[1]
-            tempRunIdentifierObject.t = tempParsedIdentifier[2]
-
-            singleExperimentDataList.append(singleExperimentData())
+            tempParsedReplicate = tempParsedIdentifier[1].split('=')
+            tempRunIdentifierObject.replicate = int(tempParsedReplicate[1])#tempParsedIdentifier[1]
+            tempParsedTime = tempParsedIdentifier[2].split('=')
+            tempRunIdentifierObject.t = float(tempParsedTime[1])#tempParsedIdentifier[2]
             for key in tempTimePointCollection:
-                tempTimePointCollection[key] = timePoint(tempRunIdentifierObject, key, tempRunIdentifierObject.time, data['titers'][i][titerNameColumn[key]])
-                if key == substrateName:
-                    print(tempTimePointCollection[key])
-                    singleExperimentDataList[-1].substrate = tempTimePointCollection[key]
-                else:
-                    singleExperimentDataList[-1].products[key] = tempTimePointCollection[key]
-
+                tempTimePointCollection[key] = timePoint(tempRunIdentifierObject, key, tempRunIdentifierObject.t, data['titers'][i][titerNameColumn[key]])
             timePointCollection.append(tempTimePointCollection.copy())
         else:
-            skippedLines = skippedLines + 1
+            skippedLines += 1
     else:
-        skippedLines = skippedLines+1
+        skippedLines += 1
+
 print("Number of lines skipped ",skippedLines)
 
+######## Combine time points into timeCourseObjects
+titerObjectList = dict()
+for timePoint in timePointCollection:
+    flag = 0
+    for titerObjectKey in titerObjectList:
+        if timePoint["Glucose"].getUniqueTimePointID() == titerObjectList[titerObjectKey]["Glucose"].getTimeCourseID(): ##We can check only one key since they should all be the same, this will be checked later
+            for key in titerObjectList[titerObjectKey]:
+                titerObjectList[titerObjectKey][key].addTimePoint(timePoint[key])
+            flag = 1
+            break
+    if flag == 0:
+        titerObjectList[timePoint[key].getUniqueTimePointID()] = dict()
+        for key in timePoint:
+            titerObjectList[timePoint[key].getUniqueTimePointID()][key] = timeCourseObject()
+            titerObjectList[timePoint[key].getUniqueTimePointID()][key].addTimePoint(timePoint[key])
+    # for x in titerObjectList[timePoint[key].getUniqueTimePointID()][key].timePointList:
+    #     print(x.t)
 
-######## Combine replicate experiments
-uniqueTimePointCollection = dict()
-#Find unique timePointIdentifiers
-for i in singleExperimentDataList:
-    if i.getUniqueTimePointID() in uniqueTimePointCollection:
-        uniqueTimePointCollection[i.getUniqueTimePointID()].append(i)
-    else:
-        uniqueTimePointCollection[i.getUniqueTimePointID()] = [i]
+# for key in titerObjectList:
+#     print(titerObjectList[key]["Glucose"].timeVec)
 
-######### Build the replicate objects
+######## Combine timeCourseObjects into singleExperimentObjects
+singleExperimentObjectList = dict()
+for key in titerObjectList:
+    singleExperimentObjectList[key] = singleExperimentData()
+    for key2 in titerObjectList[key]:
+        if key2 == substrateName:
+            singleExperimentObjectList[key].substrate = titerObjectList[key][key2]
+        else:
+            singleExperimentObjectList[key].products[key2] = titerObjectList[key][key2]
+            #print(type(singleExperimentObjectList[key].products[key2]))
+
+
+######## Combine singleExperimentObjects into replicateExperimentObjects
 replicateExperimentObjectList = dict()
-replicateExperimentObject()
+for key in singleExperimentObjectList:
+    flag = 0
+    for key2 in replicateExperimentObjectList:
+        if key2 == singleExperimentObjectList[key].getUniqueReplicateID():
+            replicateExperimentObjectList[key2].addReplicateExperiment(singleExperimentObjectList[key])
+            flag = 1
+            break
+    if flag == 0:
+        replicateExperimentObjectList[singleExperimentObjectList[key].getUniqueReplicateID()] = replicateExperimentObject()
+        tempID = singleExperimentObjectList[key].getUniqueReplicateID()
+        replicateExperimentObjectList[singleExperimentObjectList[key].getUniqueReplicateID()].addReplicateExperiment(singleExperimentObjectList[key])
 
-for key in uniqueTimePointCollection.keys():
-    replicateExperimentObjectList[key] = replicateExperimentObject()
-    for i in uniqueTimePointCollection[key]:
-        replicateExperimentObjectList[key].addReplicateExperiment(i)
+for key in replicateExperimentObjectList:#
+    print(replicateExperimentObjectList[key].avg.products["Ethanol"])
+    print(replicateExperimentObjectList[key].t)
+    print(len(replicateExperimentObjectList[key].t),len(replicateExperimentObjectList[key].avg.products["Ethanol"]))
+    plt.plot(replicateExperimentObjectList[key].t,replicateExperimentObjectList[key].avg.products["Ethanol"])
+
