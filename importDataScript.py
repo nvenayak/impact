8dde40d6c8c42b0d46d18424a50021767db4c188
Naveen <naveen.venayak@gmail.com>
2015-10-17 18:38:01 -0400

    Condensed most functions into DataObject file, including a new projectContainer

--- importDataScript.py
+++ importDataScript.py
@@ -3,80 +3,81 @@ __author__ = 'Naveen'
 ####### Import packages
 from DataObject import *
 from pyexcel_xlsx import get_data
-from dataParsingFunctions import *
 from plottingFunctions import *
-import matplotlib.pyplot as plt
-import copy
-import time
-import tkinter as tk
+import pickle
 
-####### Define some constants
-fileName = "2015.05.19.LactateStoryData.xlsx"
-substrateName = 'Glucose'
-titerDataSheetName = "titers"
+# Reparse the data, or load from old pickle?
+p1 = False
 
-####### Get data from xlsx file
-data = get_data(fileName)
+if p1 == True:
+    ####### Define some constants
+    fileName = "2015.05.19.LactateStoryData.xlsx"
+    substrateName = 'Glucose'
+    titerDataSheetName = "titers"
 
-####### Check for correct data for import
-# if 'OD' not in data.keys():
-#     raise Exception("No sheet named 'OD' found")
+    ####### Get data from xlsx file
+    data = get_data(fileName)
 
-if 'titers' not in data.keys():
-    raise Exception("No sheet named 'titers' found")
+    ####### Check for correct data for import
+    # if 'OD' not in data.keys():
+    #     raise Exception("No sheet named 'OD' found")
 
-######## Initialize variables
-titerNameColumn = dict()
-for i in range(1,len(data[titerDataSheetName][2])):
-    titerNameColumn[data[titerDataSheetName][2][i]] = i
+    if 'titers' not in data.keys():
+        raise Exception("No sheet named 'titers' found")
 
-tempTimePointCollection = dict()
-for names in titerNameColumn:
-    tempTimePointCollection[names] = []
+    ######## Initialize variables
+    titerNameColumn = dict()
+    for i in range(1,len(data[titerDataSheetName][2])):
+        titerNameColumn[data[titerDataSheetName][2][i]] = i
 
-timePointCollection = []
-skippedLines = 0
+    tempTimePointCollection = dict()
+    for names in titerNameColumn:
+        tempTimePointCollection[names] = []
 
-######## Parse the titer data into single experiment object list
-### NOTE: THIS PARSER IS NOT GENERIC AND MUST BE MODIFIED FOR YOUR SPECIFIC INPUT TYPE ###
-for i in range(4, len(data['titers'])):
-    if type("asdf") == type(data['titers'][i][0]):  #Check if the data is a string
-        tempParsedIdentifier = data['titers'][i][0].split(',')  #Parse the string using comma delimiter
-        if len(tempParsedIdentifier) >= 3:  #Ensure corect number of identifiers TODO make this general
-            tempRunIdentifierObject = runIdentifier()
-            tempParsedStrainIdentifier = tempParsedIdentifier[0].split("+")
-            tempRunIdentifierObject.strainID = tempParsedStrainIdentifier[0]
-            tempRunIdentifierObject.identifier1 = tempParsedStrainIdentifier[1]
-            # tempRunIdentifierObject.identifier2 = tempParsedIdentifier[2]
-            tempParsedReplicate = tempParsedIdentifier[1].split('=')
-            tempRunIdentifierObject.replicate = int(tempParsedReplicate[1])#tempParsedIdentifier[1]
-            tempParsedTime = tempParsedIdentifier[2].split('=')
-            tempRunIdentifierObject.t = float(tempParsedTime[1])#tempParsedIdentifier[2]
+    timePointCollection = []
+    skippedLines = 0
+    timePointList = []
+
+    ######## Parse the titer data into single experiment object list
+    ### NOTE: THIS PARSER IS NOT GENERIC AND MUST BE MODIFIED FOR YOUR SPECIFIC INPUT TYPE ###
+    for i in range(4, len(data['titers'])):
+        if type("asdf") == type(data['titers'][i][0]):  #Check if the data is a string
+            tempParsedIdentifier = data['titers'][i][0].split(',')  #Parse the string using comma delimiter
+            if len(tempParsedIdentifier) >= 3:  #Ensure corect number of identifiers TODO make this general
+                tempRunIdentifierObject = runIdentifier()
+                tempParsedStrainIdentifier = tempParsedIdentifier[0].split("+")
+                tempRunIdentifierObject.strainID = tempParsedStrainIdentifier[0]
+                tempRunIdentifierObject.identifier1 = tempParsedStrainIdentifier[1]
+                # tempRunIdentifierObject.identifier2 = tempParsedIdentifier[2]
+                tempParsedReplicate = tempParsedIdentifier[1].split('=')
+                tempRunIdentifierObject.replicate = int(tempParsedReplicate[1])#tempParsedIdentifier[1]
+                tempParsedTime = tempParsedIdentifier[2].split('=')
+                tempRunIdentifierObject.t = float(tempParsedTime[1])#tempParsedIdentifier[2]
 
 
-            for key in tempTimePointCollection:
-                tempRunIdentifierObject.titerName = key
-                if key == 'Glucose':
-                    tempRunIdentifierObject.titerType = 'substrate'
-                else:
-                    tempRunIdentifierObject.titerType = 'product'
-                tempTimePointCollection[key] = timePoint(copy.copy(tempRunIdentifierObject), key, tempRunIdentifierObject.t, data['titers'][i][titerNameColumn[key]])
-                #print(tempTimePointCollection[key].runIdentifier.titerName)
-            timePointCollection.append(tempTimePointCollection.copy())
+                for key in tempTimePointCollection:
+                    tempRunIdentifierObject.titerName = key
+                    if key == 'Glucose':
+                        tempRunIdentifierObject.titerType = 'substrate'
+                    else:
+                        tempRunIdentifierObject.titerType = 'product'
+                    timePointList.append(timePoint(copy.copy(tempRunIdentifierObject), key, tempRunIdentifierObject.t, data['titers'][i][titerNameColumn[key]]))
+
+            else:
+                skippedLines += 1
         else:
             skippedLines += 1
-    else:
-        skippedLines += 1
 
-print("Number of lines skipped: ",skippedLines)
+    print("Number of lines skipped: ",skippedLines)
 
+    # ----- Add the data to the projectContainer ----------------
+    newProjectContainer = projectContainer()
+    newProjectContainer.parseTimePointCollection(timePointList)
 
-newProjectContainer = projectContainer()
-######## Combine time points into timeCourseObjects
-titerObjectList = getTiterObjectListFromTimePointCollection(timePointCollection)
-
-# ----- Add the data to the projectContainer ----------------
-newProjectContainer.parseTiterObjectCollection(titerObjectList, 'titer')
+    # Dump the data in a pickle for storage
+    pickle.dump(newProjectContainer, open('importDataScriptPickle.p','wb'))
+else:
+    newProjectContainer = pickle.load(open('importDataScriptPickle.p','rb'))
 
 # ######## List Experiment names
 # print("Experiment Name\t# Replicates\t#Products")
@@ -85,18 +86,17 @@ newProjectContainer.parseTiterObjectCollection(titerObjectList, 'titer')
 
 
 ######## Strains To Plot
-strainsToPlotList = [['pTOG009IPTG','pTOG009aTc'],['pTOG007IPTG','pTOG007aTc'],['pTOG008IPTG','pTOG08aTc'],['pTOG0010IPTG','pTOG010aTc'],['lacI  pKDL071']]
-
-strainsToPlot = []
-for strainsToPlotPair in strainsToPlotList:
-    for strainToPlot in strainsToPlotPair:
-        strainsToPlot.append(strainToPlot)
+# strainsToPlotList = [['pTOG009IPTG','pTOG009aTc'],['pTOG007IPTG','pTOG007aTc'],['pTOG008IPTG','pTOG08aTc'],['pTOG0010IPTG','pTOG010aTc'],['lacI  pKDL071']]
+#
+# strainsToPlot = []
+# for strainsToPlotPair in strainsToPlotList:
+#     for strainToPlot in strainsToPlotPair:
+#         strainsToPlot.append(strainToPlot)
 
 strainsToPlot = ['lacI  pKDL071','pTOG009IPTG','pTOG009aTc']
-# strainsToPlot = ['pTOG009IPTG','pTOG009aTc']
 
-newProjectContainer.printGenericTimeCourse(strainsToPlot, ["Acetate","Ethanol","Lactate"])
-newProjectContainer.printEndPointYield(strainsToPlot, 1)
-newProjectContainer.printYieldTimeCourse(strainsToPlot)
+newProjectContainer.printGenericTimeCourse(strainsToPlot=strainsToPlot)
+# newProjectContainer.printEndPointYield(strainsToPlot, 1)
+# newProjectContainer.printYieldTimeCourse(strainsToPlot)
 
 plt.show()
