08309919ccdc30b5834b2082e7bc5e872fe5d13c
Naveen <naveen.venayak@gmail.com>
2017-06-18 14:50:32 -0400

    Removed pyexcel_xlsx in favor of openpyxl which is ~30x faster

--- impact/parsers.py
+++ impact/parsers.py
@@ -5,25 +5,41 @@ import time as sys_time
 from collections import OrderedDict
 
 import numpy as np
+
 from pyexcel_xlsx import get_data
+from openpyxl import load_workbook
 
 from .core.AnalyteData import TimePoint
-from .core.TrialIdentifier import ReplicateTrialIdentifier, Strain
+from .core.TrialIdentifier import TimeCourseIdentifier
 
 
 def spectromax_OD(experiment, data, id_type='CSV'):
     from .core.settings import settings
     live_calculations = settings.live_calculations
     
-    # This should be an ordered dict if imported from py_xlsx
-    assert isinstance(data, OrderedDict)
-
-
-    identifiers = data['identifiers']
-    raw_data = data['data']
+    unparsed_identifiers = [[elem.value for elem in row] for row in data['identifiers']]
+    raw_data = [[elem.value for elem in row] for row in data['data']]
 
     # The data starts at (3,2) and is in a 8x12 format
     timepoint_list = []
+
+    # Parse identifiers (to prevent parsing at every time point)
+    identifiers = []
+    for i, row in enumerate(unparsed_identifiers):
+        parsed_row = []
+        for j, data in enumerate(row):
+            if unparsed_identifiers[i][j] not in ['', 0, '0', None]:
+                temp_trial_identifier = TimeCourseIdentifier()
+
+                if id_type == 'CSV':
+                    temp_trial_identifier.parse_trial_identifier_from_csv(unparsed_identifiers[i][j])
+                elif id_type == 'traverse':
+                    temp_trial_identifier.parse_identifier(unparsed_identifiers[i][j])
+
+                parsed_row.append(temp_trial_identifier)
+            else:
+                parsed_row.append(None)
+        identifiers.append(parsed_row)
 
     for start_row_index in range(3, len(raw_data), 9):
         # print(start_row_index)
@@ -52,16 +68,15 @@ def spectromax_OD(experiment, data, id_type='CSV'):
             for i, row in enumerate(plate_data):
                 for j, data in enumerate(row):
                     # Skip wells where no identifier is listed or no data present
-                    if i<len(identifiers) and j<len(identifiers[0]) \
-                            and identifiers[i][j] not in ['',0,'0'] \
-                            and data != '':
-                        temp_trial_identifier = ReplicateTrialIdentifier()
-
-                        if id_type == 'CSV':
-                            temp_trial_identifier.parse_trial_identifier_from_csv(identifiers[i][j])
-                        elif id_type == 'traverse':
-                            temp_trial_identifier.parse_identifier(identifiers[i][j])
-
+                    if identifiers[i][j] is not None \
+                            and data not in [None,'']:
+                        # temp_trial_identifier = TimeCourseIdentifier()
+                        #
+                        # if id_type == 'CSV':
+                        #     temp_trial_identifier.parse_trial_identifier_from_csv(identifiers[i][j])
+                        # elif id_type == 'traverse':
+                        #     temp_trial_identifier.parse_identifier(identifiers[i][j])
+                        temp_trial_identifier = identifiers[i][j]
                         temp_trial_identifier.analyte_type = 'biomass'
                         temp_trial_identifier.analyte_name = 'OD600'
                         try:
@@ -87,10 +102,10 @@ def spectromax_OD_triplicate(experiment, data, id_type='CSV'):
     live_calculations = settings.live_calculations
 
     # This should be an ordered dict if imported from py_xlsx
-    assert isinstance(data, OrderedDict)
+    # assert isinstance(data, OrderedDict)
 
-    identifiers = data['identifiers']
-    raw_data = data['data']
+    identifiers = [[elem.value for elem in row] for row in data['identifiers']]
+    raw_data = [[elem.value for elem in row] for row in data['data']]
 
     # The data starts at (3,2) and is in a 8x12 format
     timepoint_list = []
@@ -134,9 +149,9 @@ def spectromax_OD_triplicate(experiment, data, id_type='CSV'):
                 for j, data in enumerate(row):
                     # Skip wells where no identifier is listed or no data present
                     if i<len(identifiers) and j<len(identifiers[0]) \
-                            and identifiers[i][j] not in ['',0,'0'] \
-                            and data != '':
-                        temp_trial_identifier = ReplicateTrialIdentifier()
+                            and identifiers[i][j] not in ['',0,'0',None] \
+                            and data not in ['',None]:
+                        temp_trial_identifier = TimeCourseIdentifier()
 
                         if id_type == 'CSV':
                             temp_trial_identifier.parse_trial_identifier_from_csv(identifiers[i][j])
@@ -181,10 +196,12 @@ def HPLC_titer_parser(experiment, data, id_type='CSV'):
     analyte_nameColumn = dict()
     titer_type = dict()
 
-    for i in range(1, len(data[titerDataSheetName][row_with_titer_names])):
-        analyte_nameColumn[data[titerDataSheetName][row_with_titer_names][i]] = i
-        titer_type[data[titerDataSheetName][row_with_titer_names][i]] = \
-            data[titerDataSheetName][row_with_titer_types][i]
+    data = [[elem.value for elem in row] for row in data[titerDataSheetName]]
+
+    for i in range(1, len(data[row_with_titer_names])):
+        analyte_nameColumn[data[row_with_titer_names][i]] = i
+        titer_type[data[row_with_titer_names][i]] = \
+            data[row_with_titer_types][i]
 
     # Initialize a timepoint_collection for each titer type (column)
     tempTimePointCollection = dict()
@@ -192,26 +209,26 @@ def HPLC_titer_parser(experiment, data, id_type='CSV'):
         tempTimePointCollection[names] = []
     skipped_lines = 0
     timepoint_list = []
-    for i in range(first_data_row, len(data['titers'])):
-        if type(data['titers'][i][0]) is str:
+    for i in range(first_data_row, len(data)):
+        if type(data[i][0]) is str:
             for key in tempTimePointCollection:
-                trial_identifier = ReplicateTrialIdentifier()
-                trial_identifier.parse_trial_identifier_from_csv(data['titers'][i][0])
+                trial_identifier = TimeCourseIdentifier()
+                trial_identifier.parse_trial_identifier_from_csv(data[i][0])
 
                 if id_type == 'CSV':
-                    trial_identifier.parse_trial_identifier_from_csv(data['titers'][i][0])
+                    trial_identifier.parse_trial_identifier_from_csv(data[i][0])
                 elif id_type == 'traverse':
-                    trial_identifier.parse_identifier(data['titers'][i][0])
+                    trial_identifier.parse_identifier(data[i][0])
 
                 trial_identifier.analyte_name = key
                 trial_identifier.analyte_type = titer_type[key]
 
-                if data['titers'][i][analyte_nameColumn[key]] == 'nan':
-                    data['titers'][i][analyte_nameColumn[key]] = np.nan
+                if data[i][analyte_nameColumn[key]] == 'nan':
+                    data[i][analyte_nameColumn[key]] = np.nan
                 timepoint_list.append(
-                    TimePoint(trial_identifier = trial_identifier,
-                              time = trial_identifier.time,
-                              data = data['titers'][i][analyte_nameColumn[key]]))
+                    TimePoint(trial_identifier=trial_identifier,
+                              time=trial_identifier.time,
+                              data=data[i][analyte_nameColumn[key]]))
 
         else:
             skipped_lines += 1
@@ -239,7 +256,7 @@ def tecan_OD(experiment, data, fileName, t0):
     skipped_lines = 0
     timeCourseObjectList = dict()
     for row in data[1:]:
-        temp_run_identifier_object = ReplicateTrialIdentifier()
+        temp_run_identifier_object = TimeCourseIdentifier()
         if type(row[0]) is str:
             temp_run_identifier_object.parse_trial_identifier_from_csv(row[0])
             temp_run_identifier_object.analyte_name = 'OD600'
@@ -257,32 +274,6 @@ def tecan_OD(experiment, data, fileName, t0):
     experiment.parse_single_trial_dict(experiment.titer_dict)
     return data, t0
 
-# def parse_raw_data(data_format, id_type='traverse',data = None, file_name = None, experiment = None):
-#     if experiment is None:
-#         from .core.Experiment import Experiment
-#         experiment = Experiment()
-#
-#     t0 = time.time()
-#
-#     if data is None:
-#         if file_name is None:
-#             raise Exception('No data or file name given to load data from')
-#
-#         # Get data from xlsx file
-#         data = get_data(file_name)
-#         print('Imported data from %s' % (file_name))
-#
-#     # Import parsers
-#     parser_case_dict = {'spectromax_OD':spectromax_OD,
-#                         'tecan_OD': tecan_OD,
-#                         'default_titers':HPLC_titer_parser
-#                         }
-#     if data_format in parser_case_dict.keys():
-#         parser_case_dict[data_format](experiment,data=data,file_name=file_name,id_type=id_type)
-#     else:
-#         raise Exception('Parser %s not found', data_format)
-#
-#     return experiment
 
 def parse_raw_data(format=None, id_type='CSV', file_name=None, data=None, experiment=None):
     if experiment is None:
@@ -299,7 +290,8 @@ def parse_raw_data(format=None, id_type='CSV', file_name=None, data=None, experi
 
         # Get data from xlsx file
         print('\nImporting data from %s...' % (file_name),end='')
-        data = get_data(file_name)
+        # data = get_data(file_name)
+        data = load_workbook(filename=file_name, data_only=True)
         print('%0.1fs' % (time.time()-t0))
 
     # Import parsers
