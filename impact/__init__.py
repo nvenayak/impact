f071b948d4fca9e17b0715c8e6591fc1c8eddc50
Naveen <naveen.venayak@gmail.com>
2017-10-18 21:48:49 -0400

    Removed testing for pythong 3.3/3.4 due to failed test

--- impact/__init__.py
+++ impact/__init__.py
@@ -3,10 +3,10 @@ __author__ = 'Naveen Venayak'
 import sys
 from warnings import warn
 
-# if sys.version_info.major < 3:
-#     warn(Exception('Require Python >= 3.5'))
-# elif sys.version_info.minor < 5:
-#     warn(Exception('Require Python >= 3.5'))
+if sys.version_info.major < 3:
+    warn(Exception('Require Python >= 3.5'))
+elif sys.version_info.minor < 5:
+    warn(Exception('Require Python >= 3.5'))
 
 # Import the core classes
 from .core import *
