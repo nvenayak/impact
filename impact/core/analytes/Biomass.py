18218b65ec156684077cad31dbef5eebacc4a595
kraj593 <kraj593@gmail.com>
2018-08-08 22:24:16 -0400

    Added a constructor for Biomass(). Otherwise it uses the base class's constructor

--- impact/core/analytes/Biomass.py
+++ impact/core/analytes/Biomass.py
@@ -1,6 +1,7 @@
 from .Base import *
 class Biomass(TimeCourse):
-    fit_type = 'gompertz'
+
+
     id = Column(Integer,ForeignKey('time_course.id'),primary_key=True)
 
     # def __init__(self):
@@ -11,6 +12,10 @@ class Biomass(TimeCourse):
     __mapper_args__ = {
         'polymorphic_identity': 'biomass',
     }
+
+    def __init__(self):
+        TimeCourse.__init__(self)
+        self.fit_type = 'gompertz'
 
     @property
     def trial_identifier(self):
