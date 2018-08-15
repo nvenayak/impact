3297113603355b2f22585d4aaec03b0928bf2b26
kraj593 <kraj593@gmail.com>
2018-08-15 12:21:02 -0400

    Added parsers for tecan_OD and tecan_OD_GFP_mCherry parsers. Also, updated tests to test for class based parsers.

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -1,9 +1,11 @@
 import unittest
 import impact
 import os
+import impact.parsers as parsers
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
 class TestParsers(unittest.TestCase):
+
     def test_generic_identifier_parser(self):
         ti = impact.ReplicateTrialIdentifier()
         ti.parse_identifier(
@@ -38,7 +40,7 @@ class TestParsers(unittest.TestCase):
 
     def test_default_HPLC_parser(self):
         expt = impact.Experiment()
-        expt.parse_raw_data('default_titers', file_name=os.path.join(BASE_DIR, 'tests/test_data/sample_input_data.xlsx'))
+        parsers.Parser.parse_raw_data(data_format='default_titers', file_name=os.path.join(BASE_DIR, 'tests/test_data/sample_input_data.xlsx'), experiment=expt, id_type='CSV')
 
         num_replicates = (len(expt.replicate_trial_dict.keys()))
 
@@ -67,7 +69,7 @@ class TestParsers(unittest.TestCase):
 
     def test_spectromax_parser(self):
         expt = impact.Experiment()
-        expt.parse_raw_data(format='spectromax_OD',id_type='traverse',file_name=os.path.join(BASE_DIR,'tests/test_data/sample_spectromax_data.xlsx'))
+        parsers.Parser.parse_raw_data(data_format='spectromax_OD', id_type='traverse', file_name=os.path.join(BASE_DIR,'tests/test_data/sample_spectromax_data.xlsx'), experiment = expt)
 
         num_replicates = (len(expt.replicate_trial_dict.keys()))
 
@@ -93,6 +95,73 @@ class TestParsers(unittest.TestCase):
         self.assertEqual(num_single_trials,88)
         self.assertEqual(num_analyte_data,88)
         self.assertEqual(num_time_points,10032)
+
+
+    def test_tecan_OD_parser(self):
+        expt = impact.Experiment()
+        parsers.Parser.parse_raw_data(data_format='tecan_OD', id_type='traverse', file_name=os.path.join(BASE_DIR,'tests/test_data/sample_tecan_OD_data.xlsx'), experiment=expt)
+
+        num_replicates = (len(expt.replicate_trial_dict.keys()))
+
+        num_single_trials = len([
+            single_trial_key for replicate_key in expt.replicate_trial_dict
+            for single_trial_key in expt.replicate_trial_dict[replicate_key].single_trial_dict
+        ])
+
+        num_analyte_data = len([
+            analyte_name for replicate_key in expt.replicate_trial_dict
+            for single_trial_key in expt.replicate_trial_dict[replicate_key].single_trial_dict
+            for analyte_name in expt.replicate_trial_dict[replicate_key].single_trial_dict[single_trial_key].analyte_dict
+        ])
+
+        num_time_points = len([
+            time_point for replicate_key in expt.replicate_trial_dict
+            for single_trial_key in expt.replicate_trial_dict[replicate_key].single_trial_dict
+            for analyte_name in expt.replicate_trial_dict[replicate_key].single_trial_dict[single_trial_key].analyte_dict
+            for time_point in expt.replicate_trial_dict[replicate_key].single_trial_dict[single_trial_key].analyte_dict[analyte_name].time_points
+        ])
+
+        self.assertEqual(num_replicates,56)
+        self.assertEqual(num_single_trials,96)
+        self.assertEqual(num_analyte_data,96)
+        self.assertEqual(num_time_points,2304)
+
+    def test_tecan_OD_reporter_parser(self):
+        expt = impact.Experiment()
+        parsers.Parser.parse_raw_data(data_format='tecan_OD_GFP_mCherry', id_type='traverse',
+                                      file_name=os.path.join(BASE_DIR,
+                                                             'tests/test_data/sample_tecan_OD_reporter_data.xlsx'),
+                                      experiment=expt)
+
+        num_replicates = (len(expt.replicate_trial_dict.keys()))
+
+        num_single_trials = len([
+            single_trial_key for replicate_key in expt.replicate_trial_dict
+            for single_trial_key in expt.replicate_trial_dict[replicate_key].single_trial_dict
+        ])
+
+        num_analyte_data = len([
+            analyte_name for replicate_key in expt.replicate_trial_dict
+            for single_trial_key in expt.replicate_trial_dict[replicate_key].single_trial_dict
+            for analyte_name in
+            expt.replicate_trial_dict[replicate_key].single_trial_dict[single_trial_key].analyte_dict
+        ])
+
+        num_time_points = len([
+            time_point for replicate_key in expt.replicate_trial_dict
+            for single_trial_key in expt.replicate_trial_dict[replicate_key].single_trial_dict
+            for analyte_name in
+            expt.replicate_trial_dict[replicate_key].single_trial_dict[single_trial_key].analyte_dict
+            for time_point in
+            expt.replicate_trial_dict[replicate_key].single_trial_dict[single_trial_key].analyte_dict[
+                analyte_name].time_points
+        ])
+
+        self.assertEqual(num_replicates, 56)
+        self.assertEqual(num_single_trials, 96)
+        self.assertEqual(num_analyte_data, 288)
+        self.assertEqual(num_time_points, 2880)
+
 
 if __name__ == '__main__':
     unittest.main()
