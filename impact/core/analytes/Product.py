6c5d20955674f42488d1a6b29e8cb3f2fa698e56
kraj593 <kraj593@gmail.com>
2018-12-21 18:50:38 +0530

    Minor change to error message

--- impact/core/analytes/Product.py
+++ impact/core/analytes/Product.py
@@ -16,6 +16,6 @@ class Product(TimeCourse):
         verbose = settings.verbose
 
         if self.trial_identifier.analyte_type == 'reporter':
-            raise Exception('Reported curve fitting not implemented')
+            raise Exception('Product curve fitting not implemented')
         else:
             raise Exception('Incorrect analyte_type')
